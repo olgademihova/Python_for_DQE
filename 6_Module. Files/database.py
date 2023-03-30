e5753ffb03290d1ae34a9c8f51bdca16f93e3e46
Volha Dzemikhava <volha_dzemikhava@epam.com>
2023-03-30 10:29:55 +0300

    some fixed DB part

--- 6_Module. Files/database.py
+++ 6_Module. Files/database.py
@@ -7,12 +7,12 @@ class DB:
         with sqlite3.connect(database_name) as self.conn:
             self.cur = self.conn.cursor()
 
-     def create(self, table_name, field1, field2, field3):
-        self.cur.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({field1}, {field2}, {field3}, CONSTRAINT unique_news UNIQUE ({field1}, {field2}, {field3}))')
+    def create(self, table_name, field1, field2, field3):
+        self.cur.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({field1}, {field2}, {field3}, CONSTRAINT unique_{table_name} UNIQUE ({field1}, {field2}, {field3}))')
         return self.cur.fetchall()
 
     def create_guess(self, table_name, field1, field2):
-        self.cur.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({field1}, {field2}, CONSTRAINT unique_news UNIQUE ({field1}, {field2}))')
+        self.cur.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({field1}, {field2}, CONSTRAINT unique_{table_name} UNIQUE ({field1}, {field2}))')
         return self.cur.fetchall()
 
     def insert_news(self, field1, field2, field3):
@@ -52,12 +52,11 @@ def save_news_to_db(database_name, news_text, news_loc, news_date):
 
 
 def save_adv_to_db(database_name, adv_text, adv_date, adv_days):
-    try:
-        dbcon = DB(database_name)
-        dbcon.create('advertising', 'description text', 'exp_date date', 'days_left real')
-        dbcon.insert_adv(adv_text, adv_date, adv_days)
-    except Exception as e:
-        print('The record exists in db', e)
+    dbcon = DB(database_name)
+    dbcon.create('advertising', 'description text', 'exp_date text', 'days_left text')
+    dbcon.insert_adv(adv_text, adv_date, adv_days)
+    # except Exception as e:
+    #     print('The record exists in db', e)
 
 
 def save_guess_to_db(database_name, guess_ask, guess_ans):
@@ -79,7 +78,7 @@ def save_guess_to_db(database_name, guess_ask, guess_ans):
 # print(select)
 #print(a[0][0])
 
-# create_adv = dbcon.create('advertising', 'description text', 'exp_date date', 'days_left real')  # call the create method
+# create_adv = dbcon.create('advertising', 'description text', 'exp_date text', 'days_left text')  # call the create method
 # insert_adv = dbcon.insert_adv('sell the bike', '2023/03/30', 7)  # call the insert_adv method
 # select_adv = dbcon.select('description', 'exp_date date', 'days_left', 'advertising')  # call the select method
 # print(select_adv)
